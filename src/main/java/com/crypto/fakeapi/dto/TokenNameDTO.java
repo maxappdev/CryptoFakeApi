746a49082cec206cc4303b53928ffd462e247a8a
Oliinyk Maksym <a11710665@unet.univie.ac.at>
2022-02-23 12:00:39 +0100

    delete paths

--- src/main/java/com/crypto/fakeapi/dto/TokenNameDTO.java
+++ src/main/java/com/crypto/fakeapi/dto/TokenNameDTO.java
@@ -11,13 +11,59 @@ import java.util.*;
 
 @Data
 public class TokenNameDTO {
+    private static String tokensString = "[\n" +
+            "  {\n" +
+            "    \"name\" : \"Bitcoin\",\n" +
+            "    \"code\" : \"BTC\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\" : \"Ethereum\",\n" +
+            "    \"code\" : \"ETH\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\" : \"Binance Coin\",\n" +
+            "    \"code\" : \"BNB\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\" : \"Tether\",\n" +
+            "    \"code\" : \"USDT\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\" : \"Cardano\",\n" +
+            "    \"code\" : \"ADA\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\" : \"Solana\",\n" +
+            "    \"code\" : \"SOL\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\" : \"USD Coin\",\n" +
+            "    \"code\" : \"USDC\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\" : \"Commemorative Token\",\n" +
+            "    \"code\" : \"MCT\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\" : \"Abele\",\n" +
+            "    \"code\" : \"ABELE\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\" : \"Aquachain\",\n" +
+            "    \"code\" : \"AQUA\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\" : \"Dogecoin\",\n" +
+            "    \"code\" : \"DOGE\"\n" +
+            "  }\n" +
+            "]";
+
     private String name;
     private String code;
 
     public static List<TokenNameDTO> findAll() throws IOException {
-        Path tokensJson = ResourceUtils.getFile("classpath:token_names.json").toPath();
         ObjectMapper mapper = new ObjectMapper();
-        TokenNameDTO[] tokens = mapper.readValue(tokensJson.toFile(), TokenNameDTO[].class);
+        TokenNameDTO[] tokens = mapper.readValue(tokensString, TokenNameDTO[].class);
         return Arrays.asList(tokens);
     }
 
